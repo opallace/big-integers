9c1fad5d3fc93d60f49ddb4d1f22ffe0f9e61f3d
wallace0x7d0 <wallace0x7d0@protonmail.com>
2020-08-31 03:01:13 -0300

    Add files via upload

--- bn.c
+++ bn.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <stdint.h>
 
 #include "bn.h"
 
+/*	Initializes the number values ​​and returns a big number pointer	
+ *
+ *  Input:
+ *  Output: a big number pointer
+ * 
+ */
 BIGNUM* init_bn(){
 	
 	BIGNUM *num = malloc(sizeof(BIGNUM));
@@ -15,9 +21,18 @@ BIGNUM* init_bn(){
 
 }
 
-int bn_to_int(BIGNUM *num){
+/*	Converts a big number to an integer	
+ *
+ *  Input: a big number pointer
+ *  Output: an integer
+ * 
+ */
+int bn_to_int(BIGNUM *nnum){
 	
 	int result = 0;
+	
+	BIGNUM *num = init_bn();
+	copy_bn(num, nnum);
 	
 	rev_bn(num);
 	for(int i = 0, e = 1; i < num->size; i++, e*=10){
@@ -31,7 +46,13 @@ int bn_to_int(BIGNUM *num){
 	return result;
 }
 
-BIGNUM* int_to_bn(int num){
+/*	Converts an integer to a big number
+ *
+ *  Input: an integer
+ *  Output: a big number pointer
+ * 
+ */
+BIGNUM* int_to_bn(uintmax_t num){
 	
 	BIGNUM *bignum = init_bn();
 
@@ -43,7 +64,7 @@ BIGNUM* int_to_bn(int num){
 	}
 	
 	do{
-		bignum->digits = realloc(bignum->digits, ++bignum->size * sizeof(int));
+		bignum->digits = realloc(bignum->digits, ++bignum->size);
 		bignum->digits[bignum->size - 1] = num % 10;
 		num /= 10;
 	
@@ -54,17 +75,58 @@ BIGNUM* int_to_bn(int num){
 	
 }
 
+/*	Calculates the bit size of a large number
+ *
+ *  Input: a big number
+ *  Output: an integer containing the length in bits
+ * 
+ */
+int bit_length_bn(BIGNUM *xx){
+
+	int result = 0;
+
+	BIGNUM *zero = int_to_bn(0);
+	BIGNUM *two  = int_to_bn(2);
+
+	BIGNUM *x = init_bn();
+	copy_bn(x , xx);
+
+	while(comp_bn(x, zero) != 0){
+		BIGNUM *div = init_bn();
+		div_bn(x, two, div);
+
+		result++;
+		copy_bn(x, div);
+
+		free_bn(div);
+	}
+
+	return result;
+}
+
+/*	Copy a big number to another big number
+ *
+ *  Input: two big numbers pointer
+ *  Output:
+ * 
+ */
 void copy_bn(BIGNUM *a, BIGNUM *b){
 	a->size = b->size;
 	a->sign = b->sign;
 	
 	free(a->digits);
-	a->digits = malloc(a->size * sizeof(int));
+	a->digits = malloc(a->size);
 	for(int i = 0; i < a->size; i++){
 		a->digits[i] = b->digits[i];
 	}
 }
 
+/*	Reverse the order of the digits of the large number
+ *
+ *  Input: a big number pointer
+ *  Output:
+ * 
+ */
 void rev_bn(BIGNUM *num){
 	for(int i = 0, j = num->size - 1; i < num->size/2-0.5; i++, j--){
 		int aux = num->digits[i];
@@ -73,16 +135,31 @@ void rev_bn(BIGNUM *num){
 	}
 }
 
+/*	Removes zeros to the left of the large number
+ *
+ *  Input: a big number pointer
+ *  Output:
+ * 
+ */
 void rmzero_bn(BIGNUM *num){
 	while(num->digits[0] == 0 && num->size > 1){
 		for(int i = 0; i < num->size - 1; i++){
 			num->digits[i] = num->digits[i+1];
 		}
 
-		num->digits = realloc(num->digits, --num->size * sizeof(int));
+		num->digits = realloc(num->digits, --num->size);
 	}
 }
 
+/*  Compares two large numbers
+ *
+ *  Input: two big numbers pointer
+ *  Output:
+ *			1 - if the first number is greater than the second
+ *    		0 - if the two numbers are equal
+ *   	   -1 - if the first number is less than the second
+ * 
+ */
 int comp_bn(BIGNUM *num1, BIGNUM *num2){
 	if(num1->size > num2->size){
 		return 1;
@@ -105,18 +182,37 @@ int comp_bn(BIGNUM *num1, BIGNUM *num2){
 	}
 }
 
+/*	Returns the highest value
+ *
+ *  Input: two integers
+ *  Output: the largest among the integers
+ * 
+ */
 int max(int x, int y){
-	if(x >= y)
+	if(x >= y){
 		return x;
+	}
 
 	return y;
 }
 
+/*  Free a big number of memory
+ *
+ *  Input: a big number pointer
+ *  Output:
+ * 
+ */
 void free_bn(BIGNUM *x){
 	free(x->digits);
 	free(x);
 }
 
+/*  Free a big number matrix from memory
+ *
+ *  Input: a big number matrix pointer
+ *  Output:
+ * 
+ */
 void free_matrix_bn(BIGNUM *matrix, int size){
 	for(int i = 0; i < size; i++){
 		free(matrix[i].digits);
@@ -125,23 +221,12 @@ void free_matrix_bn(BIGNUM *matrix, int size){
 	free(matrix);
 }
 
-void print_bn_file(FILE *file, BIGNUM *x){
-
-	for(int i = 0; i < x->size; i++){
-		fprintf(file, "%i", x->digits[i]);
-	}
-
-}
-
-void println_bn_file(FILE *file, BIGNUM *x){
-
-	for(int i = 0; i < x->size; i++){
-		fprintf(file, "%i", x->digits[i]);
-	}
-
-	fprintf(file, "\n");
-}
-
+/*  Prints a big number
+ *
+ *  Input: a big number pointer
+ *  Output:
+ * 
+ */
 void print_bn(BIGNUM *num){
 	if(num->sign){
 		printf("+");
@@ -156,6 +241,12 @@ void print_bn(BIGNUM *num){
 	}
 }
 
+/*  Prints a big number with line break
+ *
+ *  Input: a big number pointer
+ *  Output:
+ * 
+ */
 void println_bn(BIGNUM *num){
 	if(num->sign){
 		printf("+");
@@ -172,12 +263,19 @@ void println_bn(BIGNUM *num){
 	printf("\n");
 }
 
-void random_bn(BIGNUM *result){
+/*  Generates a large random number between 0 and the specified value,
+ *	[0, maximum value[.
+ *
+ *  Input: maximum value, a big number pointer
+ *  Output:
+ * 
+ */
+void random_bn(uintmax_t max_random_size, BIGNUM *result){
 	
-	int size = rand() % 2048 + 1;
+	int size = rand() % max_random_size + 1;
 	
 	BIGNUM *result_temp = malloc(sizeof(BIGNUM));
-	result_temp->digits = malloc(size * sizeof(int));
+	result_temp->digits = malloc(size);
 	result_temp->size = size;
 	result_temp->sign = 1;
 
@@ -189,12 +287,19 @@ void random_bn(BIGNUM *result){
 	free_bn(result_temp);
 }
 
+/*  Generates a large random number between the first and the second 
+ *  large number specified, [lower limit, upper limit[.
+ *
+ *  Input: lower limit, upper limit, a big number pointer
+ *  Output:
+ * 
+ */
 void random_range_bn(BIGNUM *start, BIGNUM *end, BIGNUM *result){
 	BIGNUM *sub = init_bn();
 	sub_bn(end, start, sub);
 	
 	BIGNUM *rand = init_bn();
-	random_bn(rand);
+	random_bn(sub->size + 1, rand);
 
 	BIGNUM *mod  = init_bn();
 	mod_bn(rand, sub, mod);
@@ -210,6 +315,12 @@ void random_range_bn(BIGNUM *start, BIGNUM *end, BIGNUM *result){
 	free_bn(result_temp);
 }
 
+/*  Adds two big numbers
+ *
+ *  Input: big number to be added, big number to be added, a big number pointer
+ *  Output:
+ * 
+ */
 void sum_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	BIGNUM *x = init_bn();
 	BIGNUM *y = init_bn();
@@ -240,7 +351,7 @@ void sum_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 		int r = 0;
 
 		for(int i = x->size - 1, j = y->size - 1; i >= 0; i--, j--){
-			aux->digits = realloc(aux->digits, ++aux->size * sizeof(int));
+			aux->digits = realloc(aux->digits, ++aux->size);
 
 			if(j < 0){
 				r += x->digits[i];
@@ -254,7 +365,7 @@ void sum_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 		}
 		
 		if(r != 0){
-			aux->digits = realloc(aux->digits, ++aux->size * sizeof(int));
+			aux->digits = realloc(aux->digits, ++aux->size);
 			aux->digits[aux->size - 1] = r;
 		}
 
@@ -270,6 +381,13 @@ void sum_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	free_bn(y);
 }
 
+/*  Subtracts two large numbers
+ *
+ *  Input: minuend in big number format, subtrahend in big number format, 
+ *         a big number pointer
+ *  Output:
+ * 
+ */
 void sub_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	BIGNUM *x = init_bn();
 	BIGNUM *y = init_bn();
@@ -306,7 +424,7 @@ void sub_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 		aux->sign   = x->sign;
 
 		for(int i = x->size - 1, j = y->size - 1; i >= 0; i--, j--){
-			aux->digits = realloc(aux->digits, ++aux->size * sizeof(int));	
+			aux->digits = realloc(aux->digits, ++aux->size);	
 			
 			if(j < 0){
 			
@@ -344,17 +462,23 @@ void sub_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	free_bn(y);
 }
 
+/*  Add zeros to the left to make the large number the specified size
+ *
+ *  Input: a big number pointer, integer number specifying the size that the big number will be
+ *  Output:
+ * 
+ */
 void fill(BIGNUM *x, int n){
 	if (x->size < n){
 		BIGNUM *result = init_bn();
 
 		for (int i = 0; i < (n - x->size); i++){
-			result->digits = realloc(result->digits, ++result->size * sizeof(int));
+			result->digits = realloc(result->digits, ++result->size);
 			result->digits[result->size - 1] = 0;
 		}
 
 		for (int i = 0; i < x->size; i++){
-			result->digits = realloc(result->digits, ++result->size * sizeof(int));
+			result->digits = realloc(result->digits, ++result->size);
 			result->digits[result->size - 1] = x->digits[i];
 		}
 
@@ -365,12 +489,19 @@ void fill(BIGNUM *x, int n){
 
 }
 
+/*  Split a big number between two positions
+ *
+ *  Input: a big number pointer, integer containing the starting position value, 
+ *	       integer containing the final position value, a big number pointer
+ *  Output:
+ * 
+ */
 void split(BIGNUM *x, int start, int end, BIGNUM *result){
 
 	BIGNUM *result_temp = init_bn();
 
 	for (int i = start; i < end; i++){
-		result_temp->digits = realloc(result_temp->digits, ++result_temp->size * sizeof(int));
+		result_temp->digits = realloc(result_temp->digits, ++result_temp->size);
 		result_temp->digits[result_temp->size - 1] = x->digits[i];
 	}
 
@@ -378,13 +509,19 @@ void split(BIGNUM *x, int start, int end, BIGNUM *result){
 	free_bn(result_temp);
 }
 
+/*  Performs base 10 exponentiation quickly
+ *
+ *  Input: integer containing the exponent value, a big number pointer
+ *  Output:
+ * 
+ */
 void fastpow_base10_bn(int e, BIGNUM *result){
 
 	BIGNUM *one  = int_to_bn(1);
 	BIGNUM *result_temp = int_to_bn(1);
 
 	for (int i = 0; i < e; i++){
-		result_temp->digits = realloc(result_temp->digits, ++result_temp->size * sizeof(int));
+		result_temp->digits = realloc(result_temp->digits, ++result_temp->size);
 		result_temp->digits[result_temp->size - 1] = 0;
 	}
 
@@ -394,6 +531,12 @@ void fastpow_base10_bn(int e, BIGNUM *result){
 	free_bn(result_temp);
 }
 
+/*  Performs a multiplication of big numbers using the karatsuba algorithm
+ *
+ *  Input: two big numbers that will be multiplied, a big number pointer
+ *  Output:
+ * 
+ */
 void karatsuba(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 
 	if(xx->size < 130 && yy->size < 130){
@@ -466,6 +609,12 @@ void karatsuba(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	}
 }
 
+/*  Multiplies two big numbers
+ *
+ *  Input: two big numbers that will be multiplied, a big number pointer
+ *  Output:
+ * 
+ */
 void mul_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	BIGNUM *x = init_bn();
 	BIGNUM *y = init_bn();
@@ -491,21 +640,21 @@ void mul_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 			results[num_results-1].sign = 1;
 		
 			for(int j = 0; j < k; j++){
-				results[num_results-1].digits = realloc(results[num_results-1].digits, ++results[num_results-1].size * sizeof(int));
+				results[num_results-1].digits = realloc(results[num_results-1].digits, ++results[num_results-1].size);
 				results[num_results-1].digits[results[num_results-1].size-1] = 0;
 			}
 		
 			int aux = 0;
 
 			for(int j = x->size - 1; j >= 0; j--){
-				results[num_results-1].digits = realloc(results[num_results-1].digits, ++results[num_results-1].size * sizeof(int));
+				results[num_results-1].digits = realloc(results[num_results-1].digits, ++results[num_results-1].size);
 				aux += y->digits[i] * x->digits[j];
 				results[num_results-1].digits[results[num_results-1].size-1] = aux%10;
 				aux /= 10;
 			}
 		
 			if(aux != 0){
-				results[num_results-1].digits = realloc(results[num_results-1].digits, ++results[num_results-1].size * sizeof(int));
+				results[num_results-1].digits = realloc(results[num_results-1].digits, ++results[num_results-1].size);
 				results[num_results-1].digits[results[num_results-1].size-1] = aux;
 			}
 		
@@ -529,6 +678,13 @@ void mul_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	free_bn(y);
 }
 
+/*  Divides two big numbers
+ *
+ *  Input: dividend in big number format, divisor in big number 
+ *         format, a big number pointer
+ *  Output:
+ * 
+ */
 void div_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	BIGNUM *x = init_bn();
 	BIGNUM *y = init_bn();
@@ -540,7 +696,7 @@ void div_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 
 	if(comp_bn(y, x) == 1){
 		BIGNUM *zero = int_to_bn(0);
-		copy_bn(result_temp, zero);
+		copy_bn(result, zero);
 		
 		free_bn(zero);
 	
@@ -559,7 +715,7 @@ void div_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 
 		for(int i = 0; i < x->size; i++){
 		
-			aux->digits = realloc(aux->digits, ++aux->size * sizeof(int));
+			aux->digits = realloc(aux->digits, ++aux->size);
 			aux->digits[aux->size - 1] = x->digits[i];
 			rmzero_bn(aux);
 
@@ -575,12 +731,12 @@ void div_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 					free_bn(sub);
 				}	
 		
-				result_temp->digits = realloc(result_temp->digits, ++result_temp->size * sizeof(int));
+				result_temp->digits = realloc(result_temp->digits, ++result_temp->size);
 				result_temp->digits[result_temp->size - 1] = qaux;
                              
 			}else {
 				
-				result_temp->digits = realloc(result_temp->digits, ++result_temp->size * sizeof(int));
+				result_temp->digits = realloc(result_temp->digits, ++result_temp->size);
 				result_temp->digits[result_temp->size - 1] = 0;
 
 			}
@@ -597,6 +753,13 @@ void div_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	free_bn(result_temp);
 }
 
+/*  Performs the module operation between two large numbers
+ *
+ *  Input: dividend in big number format, divisor in big number 
+ *         format, a big number pointer
+ *  Output:
+ * 
+ */
 void mod_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	BIGNUM *x = init_bn();
 	BIGNUM *y = init_bn();
@@ -611,7 +774,7 @@ void mod_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 		BIGNUM *aux = init_bn();
 
 		for(int i = 0; i < x->size; i++){
-			aux->digits = realloc(aux->digits, ++aux->size * sizeof(int));
+			aux->digits = realloc(aux->digits, ++aux->size);
 			aux->digits[aux->size - 1] = x->digits[i];
 			
 			rmzero_bn(aux);
@@ -640,6 +803,16 @@ void mod_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	free_bn(y);
 }
 
+/*  Calculates the inverse multiplicative module of two big numbers 
+ *	with the extended euclidean algorithm
+ *
+ *  Input: dividend in big number format, divisor in big number 
+ *         format, a big number pointer
+ *  Output:
+ * 
+ *	Pseudocode:
+ *
+ */
 void mod_inverse_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	
 	BIGNUM *s     = int_to_bn(0);
@@ -707,6 +880,26 @@ void mod_inverse_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
 	free_bn(zero);
 }
 
+/*  Calculates the exponentiation of a big number
+ *
+ *  Input: base in large number format, exponent in 
+ *         big number format, a big number pointer
+ *  Output:
+ * 
+ *	Pseudocode:
+ *			if expoent = 0: 
+ *				return  1;
+ *
+ *			else if expoent = 1: 
+ *				return base;
+ *
+ *			else if expoent is even: 
+ *				return pow(base * base,  expoent / 2);
+ *
+ *			else if expoent is odd: 
+ *				return base * pow(base * base, (expoent - 1) / 2);
+ *
+ */
 void pow_bn(BIGNUM *bb, BIGNUM *ee, BIGNUM *result){
 	BIGNUM *b = init_bn();
 	BIGNUM *e = init_bn();
@@ -772,6 +965,30 @@ void pow_bn(BIGNUM *bb, BIGNUM *ee, BIGNUM *result){
 	free_bn(mod);
 }
 
+/*  Calculates the exponentiation of a big number
+ *
+ *  Input: base in large number format, exponent in 
+ *         big number format, a big number pointer
+ *  Output:
+ *
+ *	Pseudocode: 
+ *		 	result = 1    
+ *
+ * 			base %= m 
+ *			      
+ *			if (base == 0): 
+ *			    return 0
+ * 			  
+ *			while (expoent > 0): 
+ *			    if ((expoent & 1) == 1): 
+ *			    	result = (result * base) % module
+ *			      
+ *			    expoent /= 2  
+ *			    base = (base * base) % module
+ *			          
+ *			return result
+ * 
+ */
 void pow_mod_bn(BIGNUM *bb, BIGNUM *ee, BIGNUM *mm, BIGNUM *result){
 	BIGNUM *b = init_bn();
 	BIGNUM *e = init_bn();
@@ -793,7 +1010,7 @@ void pow_mod_bn(BIGNUM *bb, BIGNUM *ee, BIGNUM *mm, BIGNUM *result){
 	free_bn(mod);
 
 	while(comp_bn(e, zero) == 1){
-		if(e->digits[e->size - 1] % 2 != 0){
+		if(e->digits[e->size - 1] & 1 == 1){
 			
 			BIGNUM *mul = init_bn(); 
 			mul_bn(r, b, mul);
@@ -835,44 +1052,42 @@ void pow_mod_bn(BIGNUM *bb, BIGNUM *ee, BIGNUM *mm, BIGNUM *result){
 	free_bn(r);
 }
 
+/*  calculates the greatest common divisor between two big numbers 
+ *	with the euclidean algorithm
+ *
+ *  Input: two big number pointers to calculate the mdc between 
+ *         them, a big number pointer
+ *  Output:
+ *
+ *	Pseudocode:  
+ * 				while (y != 0)
+ *	       			r = x % y
+ *	       			x = y
+ *	       			y = r
+ *				return x;
+ * 
+ */
 void mdc_bn(BIGNUM *xx, BIGNUM *yy, BIGNUM *result){
+	BIGNUM *zero = int_to_bn(0);
 	BIGNUM *x = init_bn();
 	BIGNUM *y = init_bn();
 
 	copy_bn(x, xx);
 	copy_bn(y, yy);
 
-	if(comp_bn(y, x) == 1){
-		mdc_bn(y, x, result);
+	while(comp_bn(y, zero) != 0){
 
-	}else {
-		
-		BIGNUM *zero = int_to_bn(0);
 		BIGNUM *r = init_bn();
+		mod_bn(x, y, r);
 
-		BIGNUM *x_temp = init_bn();
-		BIGNUM *y_temp = init_bn();
+		copy_bn(x, y);
+		copy_bn(y, r);
 
-		copy_bn(x_temp, x);
-		copy_bn(y_temp, y);	
-
-		do{
-			
-			mod_bn(x_temp, y_temp, r);
-			
-			copy_bn(x_temp, y_temp);
-			copy_bn(y_temp, r);
-			
-		}while(comp_bn(r, zero) != 0);
-		
-		free_bn(zero);
 		free_bn(r);
-		free_bn(y_temp);
-
-		copy_bn(result, x_temp);
-		free_bn(x_temp);
 	}
 
-	free_bn(y);
+	copy_bn(result, x);
+	free_bn(zero);
 	free_bn(x);
+	free_bn(y);
 }

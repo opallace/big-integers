646afd10666ad8c48435b503438ae25f500c9bc8
wallace0x7d0 <wallace0x7d0@protonmail.com>
2021-04-24 14:31:59 -0300

    performing tests

--- bn.c
+++ bn.c
@@ -2,22 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-//test
-#include <time.h>
-//test
-
 #include "bn.h"
 
-int main(){
-	BIGNUM *a = int_to_bn(-99);
-	BIGNUM *b = int_to_bn(-9);
-	BIGNUM *result = init_bn();
-
-	sum_bn(a, b, result);
-	println_bn(result);
-
-	return 0;
-}
 
 /*	Initializes the number values ​​and returns a big number pointer	
  *
